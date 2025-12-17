749d45178886faecb31d86bcaa2fb8378a000d0a
Aryan Ranjan <aryankumarranjan09@gmail.com>
2025-12-17 12:24:00 +0530

    git add . git commit git push

---
+++ src/Day1/Marks.java
@@ -0,0 +1,22 @@
+public class Marks {
+    public static void main(String[] args) {
+
+        int mark = 85;
+
+        char grade;
+
+        if (mark >= 90) {
+            grade = 'O';
+        } else if (mark >= 80) {
+            grade = 'E';
+        } else if (mark >= 70) {
+            grade = 'A';
+        } else if (mark >= 60) {
+            grade = 'B';
+        } else {
+            grade = 'C';
+        }
+
+        System.out.println("Grade: " + grade);
+    }
+}
